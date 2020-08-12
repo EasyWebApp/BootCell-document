fd7ffbceb3f2319a49c9b71238fa751f2906ec3a
TechQuery <shiy007@qq.com>
2020-08-12 17:34:45 +0800

    [add] PWA configuration

--- source/index.tsx
+++ source/index.tsx
@@ -1,5 +1,20 @@
+import { serviceWorkerUpdate } from 'web-utility';
 import { documentReady, render, createCell } from 'web-cell';
 
 import { PageRouter } from './page';
+
+const { serviceWorker } = window.navigator;
+
+serviceWorker
+    ?.register('sw.js')
+    .then(serviceWorkerUpdate)
+    .then(worker => {
+        if (window.confirm('New version of this Web App detected, update now?'))
+            worker.postMessage({ type: 'SKIP_WAITING' });
+    });
+
+serviceWorker?.addEventListener('controllerchange', () =>
+    window.location.reload()
+);
 
 documentReady.then(() => render(<PageRouter />));
